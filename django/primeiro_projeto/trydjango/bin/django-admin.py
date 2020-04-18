3f6eff865ec52e880fc8303207ed35857cf74a4e
Jamile Merlin <jamilemerlin@gmail.com>
2020-04-18 16:42:31 +0200

    Remove ecommerce from this repository

--- django/primeiro_projeto/trydjango/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/merlin/Desktop/jamiledev/django/primeiro_projeto/trydjango/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
